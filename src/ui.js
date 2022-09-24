4ba806be31a3e1e1a635e80369959ec905ffb570
Robin Rhodes <bigr0b@protonmail.com>
2022-09-23 23:30:07 -0400

    Add form text input through js

--- src/ui.js
+++ src/ui.js
@@ -1,4 +1,25 @@
+'use strict';
 const projectsFolder = document.querySelector('#project');
 const todoFolder = document.querySelector('#todo');
 
-export { projectsFolder, todoFolder };
+const todoForm = document.createElement('form');
+todoForm.setAttribute('id', 'todo-list-form');
+
+//form text input
+const formText = document.createElement('div');
+const textLabel = document.createElement('label');
+textLabel.setAttribute('for', 'description');
+textLabel.textContent = 'Description';
+const textInput = document.createElement('input');
+textInput.setAttribute('type', 'text');
+textInput.setAttribute('id', 'description');
+
+formText.appendChild(textLabel);
+formText.appendChild(textInput);
+
+todoForm.appendChild(formText);
+
+todoFolder.appendChild(todoForm);
+//form date input
+
+export {};
