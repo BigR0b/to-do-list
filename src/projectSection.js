f691459662a7ffb5c806b21c60af51758214abfe
Robin Rhodes <bigr0b@protonmail.com>
2022-09-30 16:22:36 -0400

    Add project from in html

--- src/projectSection.js
+++ src/projectSection.js
@@ -1,12 +1,12 @@
 const projectsList = document.querySelector('#projects-list');
-
-function findIndex() {
+const project = document.querySelector('#project');
+const findIndex = () => {
   for (let i = 0; i < projectsList.children.length; i++) {
     projectsList.children[i].addEventListener('click', function () {
       console.log(i);
     });
   }
-}
+};
 const addToProject = projects => {
   projects.forEach(project => {
     const projectItem = document.createElement('div');
@@ -15,4 +15,33 @@ const addToProject = projects => {
   });
 };
 
+//!!!! CREATE A FORM FOR PROJECTS TO CREATE A NEW PROJECT
+const createProjectFrom = () => {
+  const projectForm = document.createElement('form');
+  projectForm.setAttribute('id', 'project-list-form');
+
+  //form text input
+  const projectText = document.createElement('div');
+  const textLabel = document.createElement('label');
+  textLabel.setAttribute('for', 'form-title');
+  textLabel.textContent = 'Description';
+  const textInput = document.createElement('input');
+  textInput.setAttribute('type', 'text');
+  textInput.setAttribute('id', 'form-title');
+  textInput.required = true;
+
+  const newProjectBtn = document.createElement('button');
+  newProjectBtn.setAttribute('type', 'submit');
+  newProjectBtn.textContent = 'Select';
+  project.appendChild(newProjectBtn);
+  newProjectBtn.addEventListener('click', function (e) {
+    const isFormValid = projectForm.checkValidity();
+    if (isFormValid) {
+      e.preventDefault();
+      addFormBtn();
+      removeForm();
+      let task = textInput.value;
+    }
+  });
+};
 export { addToProject, findIndex };
